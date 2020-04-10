26ef1fa31e7faaf1877641858a8a88832a118eb6
wisnia97 <1b13wisniewski@gmail.com>
2020-04-10 12:20:39 +0200

    Fix code problems

--- app/src/main/java/com/example/rozrywkafirebase/LoginViewModel.kt
+++ app/src/main/java/com/example/rozrywkafirebase/LoginViewModel.kt
@@ -3,36 +3,17 @@ package com.example.rozrywkafirebase
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import androidx.preference.PreferenceManager
-import kotlin.random.Random
 
 class LoginViewModel : ViewModel() {
-
-    companion object {
-        val androidFacts = arrayOf(
-            "u nas możesz stworzyć własną bibliotekę filmów",
-            "u nas możesz stworzyć własną bibliotekę gier",
-            "U nas możesz stworzyć własną bibliotekę książek"
-        )
-    }
-        enum class AuthenticationState {
-            AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
-        }
-
-        val authenticationState = FirebaseUserLiveData().map { user ->
-            if (user != null) {
-                AuthenticationState.AUTHENTICATED
-            } else {
-                AuthenticationState.UNAUTHENTICATED
-            }
-        }
-
-    fun getFactToDisplay(context: Context): String {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val factTypePreferenceKey = context.getString(R.string.preference_fact_type_key)
-        val funFactType = sharedPreferences.getString(factTypePreferenceKey, null)
-
-        return androidFacts[Random.nextInt(0, androidFacts.size)]
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
     }
 
+    val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
 }
--- app/src/main/java/com/example/rozrywkafirebase/MainFragment.kt
+++ app/src/main/java/com/example/rozrywkafirebase/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.example.rozrywkafirebase
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
